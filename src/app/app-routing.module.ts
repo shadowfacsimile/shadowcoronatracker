fb5488f7b54e72578914a2faee495da5e93780db
shadowfacsimile <shadow.facsimile@gmail.com>
2020-03-14 10:21:43 +0530

    Added routing for summary and stats

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SummaryComponent } from './summary/summary.component';
+import { StatsComponent } from './stats/stats.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: SummaryComponent},
+  {path: 'summarystats', component: SummaryComponent},
+  {path: 'statsbycountry', component: StatsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
