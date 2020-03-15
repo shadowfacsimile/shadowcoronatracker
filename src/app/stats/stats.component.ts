d1c4c4e78a9fad968e107fcbc8899cc1165f749c
shadowfacsimile <shadow.facsimile@gmail.com>
2020-03-15 12:52:02 +0530

    Summary stats alignment

--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -11,6 +11,7 @@ export class StatsComponent implements OnInit {
   coronaStatsResponse;
   tempCoronaStatsResponse;
   country;
+  sort = false;
 
   constructor(private restService: RestService) { }
 
@@ -25,5 +26,59 @@ export class StatsComponent implements OnInit {
     this.coronaStatsResponse = this.tempCoronaStatsResponse;
     this.coronaStatsResponse = this.coronaStatsResponse.filter(stat => stat.country.toLowerCase().indexOf(value.toLowerCase()) >= 0);
    }
+
+  sortByCountry(value) {
+    this.sort = !this.sort;
+    this.coronaStatsResponse = this.tempCoronaStatsResponse;
+    this.coronaStatsResponse = this.coronaStatsResponse.sort((a, b) => this.sortByItem(a.country.toLowerCase(), b.country.toLowerCase()));
+  }
+
+  sortByCases(value) {
+    this.sort = !this.sort;
+    this.coronaStatsResponse = this.tempCoronaStatsResponse;
+    this.coronaStatsResponse = this.coronaStatsResponse.sort((a, b) => this.sortByItem(a.cases, b.cases));
+  }
+
+  sortByNewCases(value) {
+    this.sort = !this.sort;
+    this.coronaStatsResponse = this.tempCoronaStatsResponse;
+    this.coronaStatsResponse = this.coronaStatsResponse.sort((a, b) => this.sortByItem(a.casesSinceYesterday, b.casesSinceYesterday));
+  }
+
+  sortByDeaths(value) {
+    this.sort = !this.sort;
+    this.coronaStatsResponse = this.tempCoronaStatsResponse;
+    this.coronaStatsResponse = this.coronaStatsResponse.sort((a, b) => this.sortByItem(a.deaths, b.deaths));
+  }
+
+  sortByNewDeaths(value) {
+    this.sort = !this.sort;
+    this.coronaStatsResponse = this.tempCoronaStatsResponse;
+    this.coronaStatsResponse = this.coronaStatsResponse.sort((a, b) => this.sortByItem(a.deathsSinceYesterday, b.deathsSinceYesterday));
+  }
+
+  sortByMortalityRate(value) {
+    this.sort = !this.sort;
+    this.coronaStatsResponse = this.tempCoronaStatsResponse;
+    this.coronaStatsResponse = this.coronaStatsResponse.sort((a, b) => this.sortByItem(a.mortalityRate, b.mortalityRate));
+  }  
+
+  sortByRecoveries(value) {
+    this.sort = !this.sort;
+    this.coronaStatsResponse = this.tempCoronaStatsResponse;
+    this.coronaStatsResponse = this.coronaStatsResponse.sort((a, b) => this.sortByItem(a.recoveries, b.recoveries));
+  }
+
+  sortByRecoveryRate(value) {
+    this.sort = !this.sort;
+    this.coronaStatsResponse = this.tempCoronaStatsResponse;
+    this.coronaStatsResponse = this.coronaStatsResponse.sort((a, b) => this.sortByItem(a.recoveryRate, b.recoveryRate));
+  }
+
+  sortByItem(a, b) {
+     if((this.sort && a < b) || (!this.sort && a > b)) return 1;
+     if((this.sort && a > b) || (!this.sort && a < b)) return -1;
+     return 0;
+  }
 
 }
