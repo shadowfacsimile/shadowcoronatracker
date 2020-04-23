5ed420ad5ebf47f3a30846479cdeca378cee1c08
shadowfacsimile <shadow.facsimile@gmail.com>
2020-04-23 20:27:22 +0530

    Refactoring css and html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,9 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { ChartsModule } from 'ng2-charts';
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { StatsComponent } from './stats/stats.component';
 import { AlertsComponent } from './alerts/alerts.component';
@@ -13,7 +15,6 @@ import { CasegrowthComponent } from './casegrowth/casegrowth.component';
 import { CountrytrendComponent } from './countrytrend/countrytrend.component';
 import { CountrydeathtrendComponent } from './countrydeathtrend/countrydeathtrend.component';
 import { StatestatsComponent } from './statestats/statestats.component';
-import { RouterModule } from '@angular/router';
 import { IndiastatsComponent } from './indiastats/indiastats.component';
 import { AboutComponent } from './about/about.component';
 
--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -3,30 +3,32 @@ import { RestService } from '../services/rest.service';
 import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-summary',
-  templateUrl: './summary.component.html',
-  styleUrls: ['./summary.component.css']
+	selector: 'app-summary',
+	templateUrl: './summary.component.html',
+	styleUrls: [ './summary.component.css' ]
 })
 export class SummaryComponent implements OnInit, OnDestroy {
+	public coronaSummarySubscription: Subscription;
+	public coronaCountriesStatsSubscription: Subscription;
+	public coronaSummaryStatsResponse: any;
+	public countriesWithFirstCase: string = '';
+	public countriesWithFirstDeath: string = '';
 
-  public coronaSummarySubscription: Subscription;
-  public coronaCountriesStatsSubscription: Subscription;
-  public coronaSummaryStatsResponse: any;
-  public countriesWithFirstCase: string = '';
-  public countriesWithFirstDeath: string = '';
+	constructor(public restService: RestService) {}
 
-  constructor(public restService: RestService) { }
+	ngOnInit(): void {
+		this.coronaSummarySubscription = this.restService
+			.getCoronaSummaryStats()
+			.subscribe((data) => this.storeResponseData(data));
+	}
 
-  ngOnInit(): void {
-    this.coronaSummarySubscription = this.restService.getCoronaSummaryStats().subscribe(data => {
-      this.coronaSummaryStatsResponse = data
-      this.countriesWithFirstCase = this.coronaSummaryStatsResponse.countriesWithFirstCase.join(", ");
-      this.countriesWithFirstDeath = this.coronaSummaryStatsResponse.countriesWithFirstDeath.join(", ");
-    });
-   
-  }
+	private storeResponseData(data: any) {
+		this.coronaSummaryStatsResponse = data;
+		this.countriesWithFirstCase = this.coronaSummaryStatsResponse.countriesWithFirstCase.join(', ');
+		this.countriesWithFirstDeath = this.coronaSummaryStatsResponse.countriesWithFirstDeath.join(', ');
+	}
 
-  ngOnDestroy(): void {
-    this.coronaSummarySubscription.unsubscribe();
-  }
+	ngOnDestroy(): void {
+		this.coronaSummarySubscription.unsubscribe();
+	}
 }
